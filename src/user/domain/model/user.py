623907f42473145f1778b56ee541c667ec461079
José Márquez Doblas <jose.marquez@atlantyasoftware.com>
2021-04-08 18:57:18 +0200

    Domain event fixed to use value objects

--- src/user/domain/model/user.py
+++ src/user/domain/model/user.py
@@ -3,16 +3,33 @@ from .username import Username
 from .user_mail import UserMail
 from .user_password import UserPassword
 from .user_id import UserId
+from typing import Optional
 
 class User(Aggregate):
 
     def __init__(self, username: Username,
                  email: UserMail, password: UserPassword, *args, **kwargs):
         super(User, self).__init__(*args, **kwargs)
-        self.__username: Username = username
-        self.__email: UserMail = email
-        self.__password: UserPassword = password
+        self._username: Username = username
+        self._email: UserMail = email
+        self._password: UserPassword = password
+        
+    @property
+    def userid(self):
+        return self._id
     
+    @property
+    def username(self):
+        return self._username.value
+    
+    @property
+    def email(self):
+        return self._email.value
+    
+    @property
+    def password(self):
+        return self._password.value
+
     @classmethod
     def add(cls, userid: UserId, username: Username,
             email: UserMail, password: UserPassword) -> "User":
@@ -23,8 +40,15 @@ class User(Aggregate):
             email=email.value,
             password=password.value
         )
-    
+
     class UserWasCreated(AggregateCreated):
         username: str
         email: str
         password: str
+
+        def mutate(self, obj: Optional[Aggregate]) -> Aggregate:
+            user = super().mutate(obj)
+            user._username = Username.fromString(self.username)
+            user._password = UserPassword.fromString(self.password)
+            user._email = UserMail.fromString(self.email)
+            return user
