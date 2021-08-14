8a806be671a2e6476a29e1d2e6dba641dbf7c924
José Márquez Doblas <jose.marquez@atlantyasoftware.com>
2021-08-14 21:13:45 +0200

    Order in main.py file

--- plato/main.py
+++ plato/main.py
@@ -1,24 +1,44 @@
+
+# * DataBase configuration
 import os
-from .src.twitter.application.query.get_pending_tweets_query import GetPendingTweetsQuery
-from .src.twitter.application.query.get_pending_tweets_handler import GetPendingTweetsHandler
+if os.environ["ENV_MODE"] in ["Test"]:
+    from .DB.generate_sqlite_db import main as regenerate_db
+    regenerate_db()
+
+
+# * Dependency injection wiring
+from .src import user, brand, twitter
 from .src.user.infrastructure.user_providers import UserProviders
 from .src.brand.infrastructure.brand_providers import BrandProviders
-from .src import user, brand, twitter
+from .src.twitter.infrastructure.twitter_providers import TwitterProviders
+userProvider = UserProviders()
+userProvider.wire(packages=[user])
+brandProvider = BrandProviders()
+brandProvider.wire(packages=[brand])
+twitterProvider = TwitterProviders()
+twitterProvider.wire(packages=[twitter])
+
+
+# * Command Bus Configuration
 from .src.shared.infrastructure.plato_command_bus import PlatoCommandBus
 from .src.user.application.command.create_user_command import CreateUserCommand
 from .src.user.application.command.create_user_handler import CreateUserCommandHandler
 from .src.brand.application.command.create_brand_command import CreateBrandCommand
 from .src.brand.application.command.create_brand_handler import CreateBrandHandler
-from .src.user.infrastructure.controller.user_controller import userFlaskBlueprint
-from .src.brand.infrastructure.controller.brand_controller import brandFlaskBlueprint
-from .src.twitter.infrastructure.controller.twitter_controller import twitterFlaskBlueprint
-from .src.twitter.infrastructure.twitter_providers import TwitterProviders
 from .src.twitter.application.command.shedule_tweet_command import ScheduleTweetCommand
 from .src.twitter.application.command.shedule_tweet_handler import ScheduleTweetHandler
 from .src.twitter.application.command.add_account_command import AddAccountCommand
 from .src.twitter.application.command.add_account_handler import AddAccountHandler
 from .src.twitter.application.command.publish_tweet_command import PublishTweetCommand
 from .src.twitter.application.command.publish_tweet_handler import PublishTweetHandler
+PlatoCommandBus.subscribe(CreateUserCommand, CreateUserCommandHandler())
+PlatoCommandBus.subscribe(CreateBrandCommand, CreateBrandHandler())
+PlatoCommandBus.subscribe(AddAccountCommand, AddAccountHandler())
+PlatoCommandBus.subscribe(ScheduleTweetCommand, ScheduleTweetHandler())
+PlatoCommandBus.subscribe(PublishTweetCommand, PublishTweetHandler())
+
+
+# * Query Bus Configuration
 from .src.shared.infrastructure.plato_query_bus import PlatoQueryBus
 from .src.user.application.query.get_user_by_email_query import GetUserByEmailQuery
 from .src.user.application.query.get_user_by_email_handler import GetUserByEmailHandler
@@ -28,41 +48,29 @@ from .src.user.application.query.get_user_query import GetUserQuery
 from .src.user.application.query.get_user_handler import GetUserHandler
 from .src.brand.application.query.get_brand_by_user_id_query import GetBrandByUserIdQuery
 from .src.brand.application.query.get_brand_by_user_id_handler import GetBrandByUserIdHandler
-from flask import Flask
-from .src.shared.infrastructure.json_web_token_conf import jwtManager
-from flask_swagger_ui import get_swaggerui_blueprint
-
-if os.environ["ENV_MODE"] in ["Test"]:
-    from .DB.generate_sqlite_db import main as regenerate_db
-    regenerate_db()
-
-from .src.brand.infrastructure.read_model.on_brand_was_created import onBrandWasCreated
-from .src.twitter.infrastructure.read_model.on_account_was_added import onTwitterAccountWasCreated
-from .src.twitter.infrastructure.read_model.on_tweet_was_scheduled import onTweetWasScheduled
-from .src.twitter.infrastructure.read_model.on_tweet_was_published import onTweetWasPublished
-
-userProvider = UserProviders()
-userProvider.wire(packages=[user])
-
-brandProvider = BrandProviders()
-brandProvider.wire(packages=[brand])
-
-
-twitterProvider = TwitterProviders()
-twitterProvider.wire(packages=[twitter])
-
-PlatoCommandBus.subscribe(CreateUserCommand, CreateUserCommandHandler())
-PlatoCommandBus.subscribe(CreateBrandCommand, CreateBrandHandler())
-PlatoCommandBus.subscribe(AddAccountCommand, AddAccountHandler())
-PlatoCommandBus.subscribe(ScheduleTweetCommand, ScheduleTweetHandler())
-PlatoCommandBus.subscribe(PublishTweetCommand, PublishTweetHandler())
-
+from .src.twitter.application.query.get_pending_tweets_query import GetPendingTweetsQuery
+from .src.twitter.application.query.get_pending_tweets_handler import GetPendingTweetsHandler
 PlatoQueryBus.subscribe(GetUserByEmailQuery, GetUserByEmailHandler())
 PlatoQueryBus.subscribe(GetUserQuery, GetUserHandler())
 PlatoQueryBus.subscribe(GetBrandByUserIdQuery, GetBrandByUserIdHandler())
 PlatoQueryBus.subscribe(GetAccountQuery, GetAccountHandler())
 PlatoQueryBus.subscribe(GetPendingTweetsQuery, GetPendingTweetsHandler())
 
+
+# * Event Bus configuration
+from .src.brand.infrastructure.read_model.on_brand_was_created import onBrandWasCreated
+from .src.twitter.infrastructure.read_model.on_account_was_added import onTwitterAccountWasCreated
+from .src.twitter.infrastructure.read_model.on_tweet_was_scheduled import onTweetWasScheduled
+from .src.twitter.infrastructure.read_model.on_tweet_was_published import onTweetWasPublished
+
+
+# * Flask Configuration
+from flask import Flask
+from .src.user.infrastructure.controller.user_controller import userFlaskBlueprint
+from .src.brand.infrastructure.controller.brand_controller import brandFlaskBlueprint
+from .src.twitter.infrastructure.controller.twitter_controller import twitterFlaskBlueprint
+from .src.shared.infrastructure.json_web_token_conf import jwtManager
+from flask_swagger_ui import get_swaggerui_blueprint
 app = Flask(__name__)
 app.register_blueprint(userFlaskBlueprint)
 app.register_blueprint(brandFlaskBlueprint)
